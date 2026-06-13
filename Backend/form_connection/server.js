e450cba31b0bd06302df40e845f3b7572749959f
Ranrajen <rajenrangari863@gmail.com>
2026-06-13 19:19:32 +0530

    Backend Form connect by cors

---
+++ Backend/form_connection/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// to used the middlesware
+app.use(cors());
+app.use(express.json());
+
+//post the data
+
+app.post('/register', (req, res) => {
+  const { name, email } = req.body;
+  console.log('Name', name);
+  console.log('Email', email);
+
+  res.json({
+    success: true,
+    message: 'Registration Successfully',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
